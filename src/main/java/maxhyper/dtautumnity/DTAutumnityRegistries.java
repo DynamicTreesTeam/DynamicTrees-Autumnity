49ac095ebcb28e9211b0a00c36ec3522cf9251a0
Harley <Harleyoc1@gmail.com>
2022-08-27 13:45:08 +0100

    Update for drops rework

--- src/main/java/maxhyper/dtautumnity/DTAutumnityRegistries.java
+++ src/main/java/maxhyper/dtautumnity/DTAutumnityRegistries.java
@@ -1,16 +1,12 @@
 package maxhyper.dtautumnity;
 
-import com.ferreusveritas.dynamictrees.DynamicTrees;
 import com.ferreusveritas.dynamictrees.api.registry.TypeRegistryEvent;
-import com.ferreusveritas.dynamictrees.api.worldgen.FeatureCanceller;
 import com.ferreusveritas.dynamictrees.blocks.leaves.LeavesProperties;
-import com.ferreusveritas.dynamictrees.worldgen.cancellers.MushroomFeatureCanceller;
 import maxhyper.dtautumnity.blocks.MapleLeavesProperties;
-import net.minecraft.world.gen.feature.BigMushroomFeatureConfig;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DTAutumnityRegistries {
 
     public static void setup() {
